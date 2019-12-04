d60fbef3d07a1dbb0dccad42c86aed7fce37b008
Votizlov <seriousfox@yandex.ru>
2019-12-04 15:17:55 +0300

    както работает

--- src/Section.java
+++ src/Section.java
@@ -6,7 +6,7 @@ import static java.lang.Math.*;
 
 public class Section implements IFIgure {
     private RealPoint p1;
-    private static final double e = 0;
+    private static final double e = 1;
 
     public Section(RealPoint p1, RealPoint p2) {
         this.p1 = p1;
@@ -126,7 +126,7 @@ public class Section implements IFIgure {
         double dy = p2.getY() - p1.getY();
 
         // calculate steps required for generating pixels
-        int steps = (int) (abs(dx) > abs(dy) ? abs(dx) : abs(dy));
+        double steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
         System.out.println(steps);
 
         // calculate increment in x & y for each steps
@@ -144,7 +144,7 @@ public class Section implements IFIgure {
         double x3 = triangle.getS2().p2.getX();
         double y3 = triangle.getS2().p2.getY();
 
-        for (int i = 0; i <= steps; i++) {
+        for (double i = 0; i <= steps; i+=e) {
             double k= (x1 - X) * (y2 - y1) - (x2 - x1) * (y1 - Y);
             double m= (x2 - X) * (y3 - y2) - (x3 - x2) * (y2 - Y);
             double n= (x3 - X) * (y1 - y3) - (x1 - x3) * (y3 - Y);
